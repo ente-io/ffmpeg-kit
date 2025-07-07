59f700b0f8cdf8adc97acb99fef532d65c59a523
Prateek Sunal <prtksunal@gmail.com>
2025-07-07 18:39:31 +0530

    fix: condition

--- flutter/flutter/lib/src/ffmpeg_kit_flutter_initializer.dart
+++ flutter/flutter/lib/src/ffmpeg_kit_flutter_initializer.dart
@@ -51,7 +51,7 @@ class FFmpegKitInitializer {
   StreamSubscription? _eventSubscription;
 
   static Future<bool> initialize([bool refresh = false]) async {
-    if (!_initialized || !refresh) {
+    if (!_initialized || refresh) {
       _instance._updateEventSubscription();
     }
     if (!_initialized) {
