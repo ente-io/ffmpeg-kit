b7aac7903f70dce6d71506e221066af1a53ec7fc
Prateek Sunal <prtksunal@gmail.com>
2025-08-06 16:56:36 +0530

    fix: refresh is optional

--- flutter/flutter/lib/ffmpeg_kit.dart
+++ flutter/flutter/lib/ffmpeg_kit.dart
@@ -34,13 +34,13 @@ class FFmpegKit {
   /// Synchronously executes FFmpeg command provided. Space character is used
   /// to split command into arguments. You can use single or double quote
   /// characters to specify arguments inside your command.
-  static Future<FFmpegSession> execute(String command, bool refresh) async =>
+  static Future<FFmpegSession> execute(String command, [bool refresh = false]) async =>
       FFmpegKit.executeWithArguments(
           FFmpegKitConfig.parseArguments(command), refresh);
 
   /// Synchronously executes FFmpeg with arguments provided.
   static Future<FFmpegSession> executeWithArguments(
-      List<String> commandArguments, bool refresh) async {
+      List<String> commandArguments, [bool refresh = false]) async {
     final session =
         await FFmpegSession.create(commandArguments, null, null, null, null);
 
